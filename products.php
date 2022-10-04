c3dcb68794ddeabb2b1c802cf186596c545dd088
edivagner <edivagner@gmail.com>
2022-10-04 17:34:15 +0100

    Add files via upload

---
+++ products.php
@@ -0,0 +1,20 @@
+<?php
+
+//This code links the page with the database.
+//Make a query of the details of the products to compose the page.
+
+// Create Connection
+$conn = mysqli_connect('localhost', 'root', '', 'g00411275');
+
+$query = 'SELECT * FROM productinfo';
+
+// Get Result
+$result = mysqli_query($conn, $query);
+
+// Fetch Data
+$productinfo = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo json_encode($productinfo, JSON_INVALID_UTF8_SUBSTITUTE);
+
+// Close database connection
+mysqli_close($conn);
